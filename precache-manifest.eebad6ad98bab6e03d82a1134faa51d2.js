93ff98c503322be209e7751b48726885a555397f
Matheus <matheusaraujo.dev@gmail.com>
2021-12-09 11:26:24 -0300

    Updates

---
+++ precache-manifest.eebad6ad98bab6e03d82a1134faa51d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad09d338e512f51a1440c8c4304e46de",
+    "url": "/Projeto-Movie-Cards-Library/index.html"
+  },
+  {
+    "revision": "d379e3240aa8d72ca363",
+    "url": "/Projeto-Movie-Cards-Library/static/css/main.e2f7de8e.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/Projeto-Movie-Cards-Library/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "d379e3240aa8d72ca363",
+    "url": "/Projeto-Movie-Cards-Library/static/js/main.0d37c53d.chunk.js"
+  },
+  {
+    "revision": "15d71e931416944cbe40",
+    "url": "/Projeto-Movie-Cards-Library/static/js/runtime-main.d04b74cf.js"
+  }
+]);
